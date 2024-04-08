3be3a6d89b24ea790310c657a1dbf9c70cab908e
Lelemeline <leameline.tavier@gmail.com>
2024-04-08 14:54:22 +0200

    exercice qui fonctionne

--- TP5/exo2.c
+++ TP5/exo2.c
@@ -3,24 +3,28 @@
  * Utilisation des segments de memoire partagee                      *
  *********************************************************************/
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <wait.h>
 #include <sys/shm.h>
 #include <sys/stat.h>
 #include "segment_memoire.h"
 
-int main() {
-
+int main(int argc, char *argv[]) {
+  //définition des variables
   int pid;      // le PID du processus
   char *mem;    // pointeur vers le segment memoire
   int shmid;    // l'identificateur du segment memoire
   char *nom = (char *) malloc(15*sizeof(char));
-  nom = "exo2.c";
+  strcpy(nom, "exo2.c");
   int cle = 2 ;
   int status;
-  char chaine[50];
-  printf("Écrire un message court [<50 caractères]\n");
-  scanf("%s",&chaine);
+  if(argc!=2){
+    perror("Usage : ./exo2 texte");
+    exit(EXIT_FAILURE);
+  }
+
   // creation du segment de memoire partagee
   if((shmid = cree_segment(100,nom,cle))==-1){
     perror("Création du segment mémoire");
@@ -37,24 +41,26 @@ int main() {
     sleep(2);
     // je suis le fils!
     // a completer : s'attacher au segment et affichage de son contenu
-    shmat(shmid,&mem,SHM_RDONLY);
-    printf("contenu du segment mémoire : %s\n",mem);
+    mem =shmat(shmid,NULL,SHM_RDONLY);
+    printf("Contenu du segment mémoire : %s\n",mem);
   }
   else {
     // je suis le pere!
     // a completer : attachement et ecriture sur le segment de memoire partagee
     // a completer : attendre la fin du fils + detacher le segment et le detruire
     // ...
-    shmat(shmid,&mem,0);
+    mem = shmat(shmid,NULL,0);
+    strncpy(mem, argv[1], strlen(argv[1]));
     pid_t ret ;
     if((ret = waitpid(pid, &status, WUNTRACED ))==-1){
       perror("erreur de wait");
     }
-    sprintf(mem,chaine);
-    
+
+
   shmdt(mem);
   shmctl(shmid, IPC_RMID, 0);
   }
+  free(nom);
 
   return 0 ;
 }
